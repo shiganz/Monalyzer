38a96bd669db768e5af2533630b8b97e3943aa68
Shigan <153103221+shiganz@users.noreply.github.com>
2025-07-18 12:55:17 +0800

    Update main-content.tsx

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -7,7 +7,7 @@ export default function MainContent() {
   return (
     <div className="container mx-auto px-4 py-12">
       <h1 className="text-3xl font-bold tracking-wide bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent text-center mb-6">
-        Mon Visualizer
+        Monalyzer
       </h1>
       <TPSChart />
       <BlockAnimation />
